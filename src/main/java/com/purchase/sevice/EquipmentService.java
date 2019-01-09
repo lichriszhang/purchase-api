4f91bf2f6d12d31e3be2199724eae4a676fce52d
SuperKooli <39405686+SuperKooli@users.noreply.github.com>
2019-01-09 21:03:05 +0800

    [add] ref#12 添加对equipment的操作方法

--- src/main/java/com/purchase/sevice/EquipmentService.java
+++ src/main/java/com/purchase/sevice/EquipmentService.java
@@ -6,5 +6,13 @@ import com.purchase.entity.Equipment;
 public interface EquipmentService {
 
 	public List<Equipment> getAllEquipment();
+
+	Equipment getEquipById(String equipId);
+
+	boolean addEquip(Equipment equipment);
+	
+	boolean deleteEquipById(String equipId);
+	
+	boolean updateEquip(Equipment equipment);
 	
 }
