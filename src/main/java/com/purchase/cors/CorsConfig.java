85fae7a82d82cf0870d0fdeb35650cc2cac821dd
li.i.zhang <li.i.zhang>
2019-01-17 10:45:58 +0800

    merge feature/11

---
+++ src/main/java/com/purchase/cors/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.purchase.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")// 1 设置访问源地址
+                .allowCredentials(true)// 2 设置访问源请求头
+                .allowedMethods("GET", "POST", "DELETE", "PUT") // 3 设置访问源请求方法
+                .maxAge(3600);
+    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig()); // 4 对接口配置跨域设置
+//        return new CorsFilter(source);
+//    }
+ 
+}
