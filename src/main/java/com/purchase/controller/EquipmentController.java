f933a7ac279d940164683fb7bbf749291cd39046
li.i.zhang <li.i.zhang>
2019-01-25 17:39:56 +0800

    file upload

--- src/main/java/com/purchase/controller/EquipmentController.java
+++ src/main/java/com/purchase/controller/EquipmentController.java
@@ -1,16 +1,18 @@
 package com.purchase.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.codec.multipart.SynchronossPartHttpMessageReader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.purchase.entity.Equipment;
 import com.purchase.sevice.EquipmentService;
@@ -33,7 +35,8 @@ public class EquipmentController {
 	}
 
 	@PostMapping("/add")
-	public boolean addEquipment(Equipment equipment) {
+	public boolean addEquipment(Equipment equipment, File file) {
+		System.out.println(file.getName());
 		return equipService.addEquip(equipment);
 	}
 
